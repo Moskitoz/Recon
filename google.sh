09b9cc158445464b45e2033fae0c38fa113976bf
Moskitoz <103637170+Moskitoz@users.noreply.github.com>
2023-03-08 13:43:11 +0200

    Create google.sh

---
+++ google.sh
@@ -0,0 +1,6 @@
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
+sudo apt update
+sudo apt install google-chrome-stable 
+yes Y | command-that-asks-for-input
+google-chrome
